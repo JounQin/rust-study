6e60cfdfa62fe727b83ab2d0f927ca7ea7d540e8
JounQin <admin@1stg.me>
2023-04-26 02:16:33 +0800

    feat: finish patterns

---
+++ patterns/src/main.rs
@@ -0,0 +1,286 @@
+#![feature(exclusive_range_pattern)]
+
+fn _if_let() {
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(color) = favorite_color {
+        println!("Using your favorite color, {}, as the background", color);
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        }
+    } else {
+        println!("Using blue as the background color");
+    }
+}
+
+fn _while_let() {
+    let mut stack = Vec::new();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
+}
+
+fn _for() {
+    let v = vec!['a', 'b', 'c'];
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
+    }
+}
+
+fn print_coordinates(&(x, y): &(u32, u32)) {
+    println!("Current location: ({}, {})", x, y);
+}
+
+fn _fn() {
+    let point = (3, 5);
+    print_coordinates(&point);
+}
+
+fn _match() {
+    let x = 1;
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+}
+
+fn _match2() {
+    let x = Some(5);
+    let y = 10;
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {:?}", y), // shadowing
+        _ => println!("Default case, x = {:?}", x),
+    }
+    println!("at the end: x = {:?}, y = {:?}", x, y);
+}
+
+fn _match3() {
+    let x = 1;
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+}
+
+fn _match4() {
+    let x = 5;
+    match x {
+        1..=5 => println!("one through five"),
+        _ => println!("anything"),
+    }
+}
+
+fn _match5() {
+    let x = 'c';
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
+}
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+fn _point() {
+    let p = Point { x: 0, y: 7 };
+    let Point { x: a, y: b } = p;
+    println!("a = {}, b = {}", a, b);
+
+    let Point { x, y } = p;
+    println!("x = {}, y = {}", x, y);
+}
+
+fn _match6() {
+    let p = Point { x: 0, y: 7 };
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    }
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+fn _match7() {
+    let msg = Message::ChangeColor(0, 160, 255);
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.")
+        }
+        Message::Move { x, y } => {
+            println!("Move in the x direction {} and in the y direction {}", x, y);
+        }
+        Message::Write(text) => println!("Text message: {}", text),
+        Message::ChangeColor(r, g, b) => {
+            println!("Change the color to red {}, green {}, and blue {}", r, g, b)
+        }
+    }
+}
+
+enum Color {
+    Rgb(i32, i32, i32),
+    Hsv(i32, i32, i32),
+}
+
+enum Message2 {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(Color),
+}
+
+fn _match8() {
+    let msg = Message2::ChangeColor(Color::Hsv(0, 160, 255));
+    match msg {
+        Message2::ChangeColor(Color::Rgb(r, g, b)) => {
+            println!("Change the color to red {}, green {}, and blue {}", r, g, b)
+        }
+        Message2::ChangeColor(Color::Hsv(h, s, v)) => {
+            println!(
+                "Change the color to hue {}, saturation {}, and value {}",
+                h, s, v
+            )
+        }
+        _ => (),
+    }
+}
+
+fn _match9() {
+    let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
+}
+
+fn foo(_: i32, y: i32) {
+    println!("This code only uses the y parameter: {}", y);
+}
+
+fn _ignore() {
+    foo(3, 4);
+}
+
+fn _match10() {
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+    println!("setting is {:?}", setting_value);
+}
+
+fn _match11() {
+    let numbers = (2, 4, 8, 16, 32);
+    match numbers {
+        (first, _, third, _, fifth) => {
+            println!("Some numbers: {}, {}, {}", first, third, fifth)
+        }
+    }
+}
+
+fn _match12() {
+    let s = Some(String::from("Hello!"));
+
+    if let Some(_ /* _s */) = s {
+        println!("found a string");
+    }
+
+    println!("{:?}", s);
+}
+
+struct Point2 {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+fn _match13() {
+    let origin = Point2 { x: 0, y: 0, z: 0 };
+    match origin {
+        Point2 { x, .. } => println!("x is {}", x),
+    }
+}
+
+fn _match14() {
+    let numbers = (2, 4, 8, 16, 32);
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {}, {}", first, last)
+        }
+    }
+}
+
+fn _match15() {
+    let num = Some(4);
+    match num {
+        Some(x) if x % 2 == 0 => println!("Even"),
+        Some(_) => println!("Odd"),
+        None => (),
+    }
+}
+
+fn _match16() {
+    let x = Some(5);
+    let y = 10;
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {:?}", n),
+        _ => println!("Default case, x = {:?}", x),
+    }
+    println!("at the end: x = {:?}, y = {:?}", x, y);
+}
+
+fn _match17() {
+    let x = 4;
+    let y = false;
+    match x {
+        4 | 5 | 6 if y => println!("yes"),
+        _ => println!("no"),
+    }
+}
+
+enum Message3 {
+    Hello { id: i32 },
+}
+
+fn _match18() {
+    let msg = Message3::Hello { id: 5 };
+
+    match msg {
+        Message3::Hello {
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {}", id_variable),
+        Message3::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        Message3::Hello { id } => println!("Found some other id: {}", id),
+    }
+}
+
+fn main() {
+    _match18();
+}
